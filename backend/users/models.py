5145c7e112d1e9ed6610ed93a6e023d611183fb9
Vladislav <vladpronko88@gmail.com>
2022-04-29 16:25:22 +0300

    add unique cinstraints to Follow model

--- backend/users/models.py
+++ backend/users/models.py
@@ -47,6 +47,9 @@ class Follow(models.Model):
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
+        constraints = [models.UniqueConstraint(
+            fields=['user', 'author'], name='unique_follow')
+        ]
 
     def __str__(self):
         return (f'Подписчик: { self.user }\n'
