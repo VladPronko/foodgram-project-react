e21a6396d54fe385d63be6059abd07edfbcc08eb
Vlad Pronko <vladpronko@MacBook-Air-Vlad.local>
2022-04-30 16:10:43 +0300

    settings update

--- backend/foodproject/settings.py
+++ backend/foodproject/settings.py
@@ -128,15 +128,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Media files (Recipes images)
 
-MEDIA_URL = "/backend_media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "backend_media")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
