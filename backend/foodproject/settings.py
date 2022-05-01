f976a5a7f88ae75affb8b6ad47865ad47d5ffc5f
Vlad Pronko <vladpronko@MacBook-Air-Vlad.local>
2022-05-02 01:35:19 +0300

    finally

--- backend/foodproject/settings.py
+++ backend/foodproject/settings.py
@@ -11,6 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='sh+fixo6u(3p3dk77^051tld-9p+oye7y(h6x2w-5s@70a5mwa')
 # SECRET_KEY = 'sh+fixo6u(3p3dk77^051tld-9p+oye7y(h6x2w-5s@70a5mwa'
