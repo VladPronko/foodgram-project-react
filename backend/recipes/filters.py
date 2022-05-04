a467b6f090fb98c5be5208adb191efd30c1251e5
Vladislav <88838807+VladPronko@users.noreply.github.com>
2022-05-04 19:49:00 +0300

    Update filters.py

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,7 +1,13 @@
+from enum import Enum
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import AllValuesMultipleFilter
 
 from .models import Recipe
+
+
+class IsFavorited(Enum):
+    IN = 1
+    NOT_IN = 0
 
 
 class RecipeFilter(filters.FilterSet):
@@ -10,12 +16,16 @@ class RecipeFilter(filters.FilterSet):
     по 'вычисляемым на ходу полям': 'is_favorited' и 'is_in_shopping_cart'.
     """
     tags = AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(
-        field_name='favorites'
-    )
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         field_name='shopping_cart'
     )
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value == IsFavorited.IN.value and user.is_authenticated:
+            return queryset.filter(favorites__user=user)
+        return queryset
 
     class Meta:
         model = Recipe
